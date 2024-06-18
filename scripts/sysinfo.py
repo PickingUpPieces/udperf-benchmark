4e36183857a7bce5d6410ffb2ee44e99114f46a0
Vincent Picking <v.picking@gmx.de>
2024-06-18 17:19:43 +0200

    Fix sysinfo script

--- scripts/sysinfo.py
+++ scripts/sysinfo.py
@@ -25,6 +25,7 @@ def execute_command(command: str):
 def main():
     parser = argparse.ArgumentParser(description="Retrieving system information of host")
     parser.add_argument("interface", type=str, help="The network interface")
+    parser.add_argument("ip", type=str, help="The IP address of the network interface")
     args = parser.parse_args()
 
     logging.info(f'Interface: {args.interface}')
