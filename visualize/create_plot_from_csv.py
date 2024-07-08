c53158379b524a164499966a238730302a48f060
Vincent Picking <v.picking@gmx.de>
2024-07-08 17:48:38 +0200

    Add replace feature for create_plot script; Create more compact pdfs; Remove small font for bar graph

--- visualize/create_plot_from_csv.py
+++ visualize/create_plot_from_csv.py
@@ -45,7 +45,7 @@ def parse_results_file(results_file):
     logging.info('Read %s test results', len(results))
     return results
 
-def generate_area_chart(x: str, y: str, data, chart_title: str, results_file: str, results_folder: str, add_labels=False, rm_filename=False, no_errors=False, pdf=False):
+def generate_area_chart(x: str, y: str, data, chart_title: str, results_file: str, results_folder: str, add_labels=False, rm_filename=False, no_errors=False, pdf=False, replace_plot=False):
     plt.figure()
 
     for test in data:
@@ -98,7 +98,7 @@ def generate_area_chart(x: str, y: str, data, chart_title: str, results_file: st
 
     chart_title = chart_title.lower().replace(" - ", "_").replace(" ", "_").replace("/", "_").replace("-", "_")
     plot_file = results_folder + "/" + chart_title + '_area'
-    save_plot(plot_file, pdf)
+    save_plot(plot_file, pdf, replace_plot)
 
 def generate_heatmap(x: str, y: str, test_name, data, chart_title, results_file, results_folder: str, rm_filename=False):
     logging.debug('Generating heatmap for %s', test_name)
@@ -162,7 +162,7 @@ def generate_heatmap(x: str, y: str, test_name, data, chart_title, results_file,
     plt.close()
 
 
-def generate_bar_chart(y: str, data, chart_title: str, results_file, results_folder: str, rm_filename=False, no_errors=False, x_label=None, pdf=False):
+def generate_bar_chart(y: str, data, chart_title: str, results_file, results_folder: str, rm_filename=False, no_errors=False, x_label=None, pdf=False, replace_plot=False):
     # Map every row in the data as a bar with the y value
     logging.debug("Generating bar chart for %s with data %s", y, data)
 
@@ -197,7 +197,7 @@ def generate_bar_chart(y: str, data, chart_title: str, results_file, results_fol
     if x_label is not None:
         plt.xlabel(MAPPINGS_COLUMNS.get(x_label, x_label))
     plt.ylabel(MAPPINGS_COLUMNS.get(y, y))
-    plt.xticks(fontsize='x-small')  # rotation=20, ha="right",  Rotate labels for readability
+    #plt.xticks(fontsize='x-small')  # rotation=20, ha="right",  Rotate labels for readability
 
     if not rm_filename:
         plt.text(0.99, 0.5, "data: " + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
@@ -205,34 +205,36 @@ def generate_bar_chart(y: str, data, chart_title: str, results_file, results_fol
 
     chart_title = chart_title.lower().replace(" - ", "_").replace(" ", "_").replace("/", "_").replace("-", "_")
     plot_file = results_folder + "/" + chart_title + '_bar'
-    save_plot(plot_file, pdf)
+    save_plot(plot_file, pdf, replace_plot)
 
     
-def save_plot(plot_file, pdf):
-    counter = 1
-    plot_file_search = plot_file
-    if pdf:
-        plot_file_search += '.pdf'
-    else:
-        plot_file_search += '.png'
-    logging.info('Saving plot to %s', plot_file)
-    plot_file_new = plot_file
-
-    while os.path.exists(plot_file_search):
-        plot_file_new = f'{plot_file}-{counter}'
-        counter += 1
-        plot_file_search = plot_file_new
+def save_plot(plot_file, pdf, replace_plot=False):
+    if replace_plot is False:
+        counter = 1
+        plot_file_search = plot_file
         if pdf:
             plot_file_search += '.pdf'
         else:
             plot_file_search += '.png'
-        
+        logging.info('Saving plot to %s', plot_file)
+        plot_file_new = plot_file
 
+        while os.path.exists(plot_file_search):
+            plot_file_new = f'{plot_file}-{counter}'
+            counter += 1
+            plot_file_search = plot_file_new
+            if pdf:
+                plot_file_search += '.pdf'
+            else:
+                plot_file_search += '.png'
+    else:
+        plot_file_new = plot_file
+        
     if pdf:
         plot_file_new += '.pdf'
     else:
         plot_file_new += '.png'
-    plt.savefig(plot_file_new)
+    plt.savefig(plot_file_new, bbox_inches='tight', pad_inches=0.1)
     logging.info('Saved plot to %s', plot_file_new)
     plt.close()
     
@@ -291,6 +293,7 @@ def main():
     parser.add_argument('--no-errors', action="store_true", help='Dont display errors (standard deviation etc.) in the charts')
     parser.add_argument('--x-label', default=None, help='Label for the x-axis in the bar chart')
     parser.add_argument('--pdf', action="store_true", help='Save the plots as pdf')
+    parser.add_argument('--replace', action="store_true", help='Replace the existing plot file')
     args = parser.parse_args()
 
     logging.info('Reading results file: %s', args.results_file)
@@ -298,13 +301,13 @@ def main():
     logging.debug('Results: %s', results)
 
     if args.type == 'area':
-        generate_area_chart(args.x_axis_param, args.y_axis_param, results, args.chart_name, args.results_file, args.results_folder, args.l, args.rm_filename, args.no_errors, args.pdf)
+        generate_area_chart(args.x_axis_param, args.y_axis_param, results, args.chart_name, args.results_file, args.results_folder, args.l, args.rm_filename, args.no_errors, args.pdf, args.replace)
     elif args.type == 'bar':
         for test in results:
             # If no chart name supplied, take the test name
             if args.chart_name == "Benchmark":
                 args.chart_name = test[0]["test_name"] 
-            generate_bar_chart(args.y_axis_param, test, args.chart_name, args.results_file, args.results_folder, args.rm_filename, args.no_errors, args.x_label, args.pdf)
+            generate_bar_chart(args.y_axis_param, test, args.chart_name, args.results_file, args.results_folder, args.rm_filename, args.no_errors, args.x_label, args.pdf, args.replace)
     elif args.type == 'heat':
         generate_heatmap(args.x_axis_param, args.y_axis_param, args.test_name, results, args.chart_name, args.results_file, args.results_folder, args.rm_filename)
 
