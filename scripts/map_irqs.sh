992b8b28787f54a01084b1669dd48d6e0d7216c0
Vincent Picking <v.picking@gmx.de>
2024-07-16 19:22:10 +0200

    Fix map_irqs

--- scripts/map_irqs.sh
+++ scripts/map_irqs.sh
@@ -33,38 +33,34 @@ remove_existing_rules() {
     done <<< "$(sudo ethtool -n $INTERFACE 2>/dev/null)"
 }
 
-# Function to calculate CPU mask for a single core
-calculate_cpu_mask_for_core() {
-    local core_index=$1
-    local mask=$((1 << core_index))
-    printf "%x" "$mask"
-}
 
 # 1. Map interrupts to of queues 0-11 to cores 12-23 
 map_interrupts() {
-    for queue_num in {0..11}; do
-        # Calculate the corresponding core for the queue (12-23 for queues 0-11)
-        local core_id=$((queue_num + 12))
-        # Calculate the CPU mask for the core
-        local cpu_mask=$(calculate_cpu_mask_for_core $core_id)
-        # Find the IRQ number for the queue
-        local irq_num=$(grep -E "TxRx-$queue_num" /proc/interrupts | cut -d: -f1 | tr -d ' ')
-        # Set the smp_affinity for the IRQ to the CPU mask
-        echo "Setting IRQ $irq_num (Queue $queue_num) affinity to core $core_id (mask $cpu_mask)"
-        echo $cpu_mask > /proc/irq/$irq_num/smp_affinity
+    local start_core=12
+    local end_core=23
+    local irq_count=0
+    local cpu_core=$((start_core + 1))
+
+    for irq in $(grep $INTERFACE /proc/interrupts | awk '{print $1}' | tr -d ':'); do
+        if [[ $irq_count -lt 12 ]]; then
+            echo "Setting affinity for IRQ $irq to CPU core $cpu_core"
+            sudo sh -c "echo $((1 << cpu_core)) > /proc/irq/$irq/smp_affinity"
+            cpu_core=$((cpu_core + 1))
+            irq_count=$((irq_count + 1))
+        else
+            break
+        fi
     done
 }
 
-# 2. Map traffic generated on cpu cores 0-11 to queues 0-11
 configure_xps() {
     local start_core=0
     local end_core=11
-    local total_cores=$((end_core - start_core + 1))
     local tx_queues=(/sys/class/net/$INTERFACE/queues/tx-*)
 
     for queue_index in {0..11}; do
-        local assigned_core=$((start_core + queue_index % total_cores))
-        local cpu_mask=$(calculate_cpu_mask_for_core $assigned_core)
+        local assigned_core=$((start_core + queue_index))
+        local cpu_mask=$((1 << assigned_core))
         local txq=${tx_queues[$queue_index]}
         echo "Setting XPS for $txq to CPU mask $cpu_mask (Core $assigned_core)"
         echo $cpu_mask > "$txq"/xps_cpus
@@ -121,15 +117,15 @@ END_CORE_ID=$((START_CORE_ID + 11))
 # Disable and stop irqbalance
 disable_irqbalance
 
-# Remove all existing n-tuple rules
-remove_existing_rules
-
 
 # Configure RSS or XPS based on the specified core range
 if [ "$START_CORE_ID" -eq 12 ]; then
     map_interrupts
     configure_xps 
 else
+    # Remove all existing n-tuple rules
+    remove_existing_rules
+
     configure_rss $START_CORE_ID $END_CORE_ID
 fi
 
