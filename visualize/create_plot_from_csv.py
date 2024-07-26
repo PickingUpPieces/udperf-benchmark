f7339348bd90ac3fd8be3abfae7ca531c80f7dbd
Vincent Picking <v.picking@gmx.de>
2024-07-26 13:08:09 +0200

    Fix script

--- visualize/create_plot_from_csv.py
+++ visualize/create_plot_from_csv.py
@@ -61,15 +61,18 @@ def pre_process_data(results_file: str) -> pd.DataFrame:
         processed_groups.append(group)
     
     processed_df = pd.concat(processed_groups)
-    logging.debug("Processed data: %s", processed_df)
 
+    # Ensure run_name is always a string
+    processed_df['run_name'] = processed_df['run_name'].astype(str)
+
+    logging.debug('Processed data: %s', processed_df)
     return processed_df
 
 
-def get_names_ordered(results_file: str, name: str) -> list:
+def get_names_ordered(results_file: str, name: str) -> np.ndarray:
     data = pd.read_csv(results_file)
-    # Get the unique test_names in the order they appear -> This order should be the order in the plot
-    return data[name].unique().tolist()
+    # Get the unique names in the order they appear in the csv file -> This order should be the order in the plot
+    return data[name].unique()
 
 
 def generate_area_chart(x: str, y: str, data: pd.DataFrame, chart_title: str, results_file: str, results_folder: str, add_labels=False, rm_filename=False, no_errors=False, pdf=False, replace_plot=False, plot_per_repetition=False):
@@ -77,12 +80,12 @@ def generate_area_chart(x: str, y: str, data: pd.DataFrame, chart_title: str, re
  
     # Ensure data is ordered but iterate in the same order as the test names appear in the results file
     grouped = data.groupby('test_name')
-    test_names_ordered = get_names_ordered(results_file, "test_name")
+    test_names_ordered = get_names_ordered(results_file, 'test_name')
 
-    for test_name in test_names_ordered:
+    for test_name in test_names_ordered.tolist():
         group = grouped.get_group(test_name)
         grouped_by_x = group.groupby(x)
-        logging.info("Grouped by %s with amount elements %s", x, len(grouped_by_x))
+        logging.info('Grouped by %s with number of tests %s', x, len(grouped_by_x))
 
         # Calculate mean and standard deviation 
         mean_y = grouped_by_x[y].mean()
@@ -108,12 +111,12 @@ def generate_area_chart(x: str, y: str, data: pd.DataFrame, chart_title: str, re
     plt.ylabel(MAPPINGS_COLUMNS.get(y, y))
     plt.ylim(bottom=0)  # Set the start of y-axis to 0
     if not rm_filename:
-        plt.text(0.99, 0.5, "data: " + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
+        plt.text(0.99, 0.5, 'data: ' + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
     plt.title(chart_title)
     plt.legend()
 
-    chart_title = chart_title.lower().replace(" - ", "_").replace(" ", "_").replace("/", "_").replace("-", "_")
-    plot_file = results_folder + "/" + chart_title + '_area'
+    chart_title = chart_title.lower().replace(' - ', '_').replace(' ', '_').replace('/', '_').replace('-', '_')
+    plot_file = results_folder + '/' + chart_title + '_area'
     save_plot(plot_file, pdf, replace_plot)
 
 
@@ -150,7 +153,7 @@ def generate_heatmap(x: str, y: str, test_name, data, chart_title, results_file,
     logging.debug('DataFrame: %s', df)
 
     # Devide each value through the thread_amount to get the value per thread
-    if x.startswith("amount_threads"):
+    if x.startswith('amount_threads'):
         df['Value'] = df.apply(lambda row: row['Value'] / row[x], axis=1)
 
     logging.debug('DataFrame: %s', df)
@@ -166,15 +169,15 @@ def generate_heatmap(x: str, y: str, test_name, data, chart_title, results_file,
 
     # Generate heatmap
     plt.figure(figsize=(10, 8))
-    sns.heatmap(pivot_table, cmap="YlGnBu", linewidths=.5, fmt='g')
+    sns.heatmap(pivot_table, cmap='YlGnBu', linewidths=.5, fmt='g')
     plt.xlabel(x)
     plt.ylabel(y)
     if not rm_filename:
-        plt.text(0.99, 0.5, "data: " + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
+        plt.text(0.99, 0.5, 'data: ' + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
     plt.title(chart_title)
 
-    chart_title = chart_title.lower().replace(" - ", "_").replace(" ", "_").replace("/", "_").replace("-", "_")
-    plt.savefig(results_folder + "/" + chart_title + '_heatmap.png')
+    chart_title = chart_title.lower().replace(' - ', '_').replace(' ', '_').replace('/', '_').replace('-', '_')
+    plt.savefig(results_folder + '/' + chart_title + '_heatmap.png')
     logging.info('Saved plot to %s_heatmap.png', chart_title)
     plt.close()
 
@@ -183,16 +186,15 @@ def generate_heatmap(x: str, y: str, test_name, data, chart_title, results_file,
 def generate_bar_chart(y: str, data: pd.DataFrame, chart_title: str, results_file, results_folder: str, rm_filename=False, no_errors=False, x_label=None, pdf=False, replace_plot=False, plot_per_repetition=False):
     # In the case of the bar chart, we expect run_name is the unique value differentiating the runs 
     # We assume that test_name is the same for all runs
+    data['run_name'] = pd.Categorical(data['run_name'], categories=get_names_ordered(results_file, 'run_name'), ordered=True)
+
     grouped = data.groupby('run_name')[y]
 
     mean_std = grouped.agg(['mean', 'std'])
     grouped_data = mean_std.reset_index()
 
-    # String is type object in pandas -> run_name could be int or string, therefore we need to check the type
-    if grouped_data['run_name'].dtype == 'object':
-        plot_x_values = grouped_data['run_name'].str.replace(" ", "\n", 1)  # Enhance readability
-    else:
-        plot_x_values = grouped_data['run_name']
+    # Replacing spaces in x-axis labels for better readability
+    plot_x_values = grouped_data['run_name'].str.replace(' ', '\n', 1) 
 
     # Calculate mean and standard dev for each run
     means = grouped_data['mean']
@@ -208,15 +210,15 @@ def generate_bar_chart(y: str, data: pd.DataFrame, chart_title: str, results_fil
     plt.ylabel(MAPPINGS_COLUMNS.get(y, y))
 
     if len(plot_x_values) > 4:
-        logging.info("Rotating x-axis labels")
-        plt.xticks(fontsize="small", rotation=25)
+        logging.info('Rotating x-axis labels')
+        plt.xticks(fontsize='small', rotation=25)
 
     if not rm_filename:
-        plt.text(0.99, 0.5, "data: " + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
+        plt.text(0.99, 0.5, 'data: ' + os.path.basename(results_file), ha='center', va='center', rotation=90, transform=plt.gcf().transFigure, fontsize=8)
     plt.title(chart_title)
 
-    chart_title = chart_title.lower().replace(" - ", "_").replace(" ", "_").replace("/", "_").replace("-", "_")
-    plot_file = results_folder + "/" + chart_title + '_bar'
+    chart_title = chart_title.lower().replace(' - ', '_').replace(' ', '_').replace('/', '_').replace('-', '_')
+    plot_file = results_folder + '/' + chart_title + '_bar'
     save_plot(plot_file, pdf, replace_plot)
 
     
@@ -254,19 +256,19 @@ def main():
     logging.debug('Starting main function')
 
     parser = argparse.ArgumentParser(description='Plot generation for nperf benchmarks.')
-    parser.add_argument('results_file', default="nperf-output.csv", help='Path to the CSV file to get the results.')
+    parser.add_argument('results_file', default='nperf-output.csv', help='Path to the CSV file to get the results.')
     parser.add_argument('--results-folder', default=PATH_TO_RESULTS_FOLDER, help='Folder to save the generated plots')
-    parser.add_argument('chart_name', default="Benchmark", help='Name of the generated chart')
-    parser.add_argument('x_axis_param', default="run_name", help='Name of the x-axis parameter')
-    parser.add_argument('y_axis_param', default="data_rate_gbit", help='Name of the y-axis parameter')
+    parser.add_argument('chart_name', default='Benchmark', help='Name of the generated chart')
+    parser.add_argument('x_axis_param', default='run_name', help='Name of the x-axis parameter')
+    parser.add_argument('y_axis_param', default='data_rate_gbit', help='Name of the y-axis parameter')
     parser.add_argument('--test_name', help='Name of the specific test to generate the heatmap for')
-    parser.add_argument('type', default="area", help='Type of graph to generate (area, bar, heat)')
-    parser.add_argument('-l', action="store_true", help='Add labels to data points')
-    parser.add_argument('--rm-filename', action="store_true", help='Add the results file name to the graph')
-    parser.add_argument('--no-errors', action="store_true", help='Dont display errors (standard deviation etc.) in the charts')
+    parser.add_argument('type', default='area', help='Type of graph to generate (area, bar, heat)')
+    parser.add_argument('-l', action='store_true', help='Add labels to data points')
+    parser.add_argument('--rm-filename', action='store_true', help='Add the results file name to the graph')
+    parser.add_argument('--no-errors', action='store_true', help='Dont display errors (standard deviation etc.) in the charts')
     parser.add_argument('--x-label', default=None, help='Label for the x-axis in the bar chart')
-    parser.add_argument('--pdf', action="store_true", help='Save the plots as pdf')
-    parser.add_argument('--replace', action="store_true", help='Replace the existing plot file')
+    parser.add_argument('--pdf', action='store_true', help='Save the plots as pdf')
+    parser.add_argument('--replace', action='store_true', help='Replace the existing plot file')
 
     args = parser.parse_args()
 
@@ -276,7 +278,7 @@ def main():
     if args.type == 'area':
         generate_area_chart(args.x_axis_param, args.y_axis_param, data_frame, args.chart_name, args.results_file, args.results_folder, args.l, args.rm_filename, args.no_errors, args.pdf, args.replace)
     elif args.type == 'bar':
-        if args.chart_name == "Benchmark":
+        if args.chart_name == 'Benchmark':
             args.chart_name = data_frame['test_name'].iloc[0]
         generate_bar_chart(args.y_axis_param, data_frame, args.chart_name, args.results_file, args.results_folder, args.rm_filename, args.no_errors, args.x_label, args.pdf, args.replace)
     elif args.type == 'heat':
